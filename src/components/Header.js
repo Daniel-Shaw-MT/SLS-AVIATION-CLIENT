c7e9547d5b0e176f066436324f966fb30a6ddcfd
Daniel Shaw <60759034+Daniel-Shaw-MT@users.noreply.github.com>
2021-01-23 19:49:40 +0100

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className='main-container'>
+                <h1 className='title'>Login or signup</h1>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
